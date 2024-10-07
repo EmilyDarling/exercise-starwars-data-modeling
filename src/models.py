436c62426f22a109c78bc95f702b6cc825d72052
Angelina <angelaaquin13@gmail.com>
2024-10-07 23:42:18 +0000

    initial

--- src/models.py
+++ src/models.py
@@ -28,5 +28,57 @@ class Address(Base):
     def to_dict(self):
         return {}
 
+
+class User(Base):
+    __tablename__ = 'user'   
+    id = Column(Integer, primary_key=True)
+    user_name= Column(String(250), nullable=False)
+    password= Column(String(250), nullable=False)
+
+
+
+class Character(Base):
+    __tablename__ = 'character'   
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    height = Column(String(250), nullable=False)
+    skin_color = Column(String(250), nullable=False)
+    mass = Column(String(250), nullable=False)
+    
+
+class Planet(Base):
+    __tablename__ = 'planet'  
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+    gravity = Column(String(250), nullable=False)
+    rotation_period = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+    mass = Column(String(250), nullable=False)
+    
+
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+   
+    id = Column(Integer, primary_key=True)  
+    name = Column(String(250), nullable=False) 
+    user_id = Column(Integer, ForeignKey('user.id'))
+    person = relationship(User)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    character = relationship(Character)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planet)
+
+
+
+    
+
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
